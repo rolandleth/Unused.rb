99ce014b25d042f4685aa422742d1868feda07db
Roland Leth <roland@rolandleth.com>
2014-02-01 16:27:24 +0200

    Huge improvements, read extended please.
    Split excluded into excluded_folders and excluded_files. Added
    imagesets with a default value of ['.imageset']. Add them here, if you
    use several Image Sets.
    Properly searches and deletes imagesets.
    Properly ignores appiconset and launchimages.
    Properly searches for name occurrences: removed the .downcase to be
    more exact. No idea why I used it, it was just dumb; other minor
    improvements too.
    It now removes references to the deleted (stand-alone) images from your
    pbxproj file; imagesets are automatically handled by Xcode.

--- unused.rb
+++ unused.rb
@@ -2,6 +2,9 @@ require 'pathname'
 require 'fileutils'
 
 location = ARGV.first
+VERBOSE = false 
+# In case you wonder why a certain image is marked as unused,
+# set to true and it will show what images are found in which file.
 loop do
 	begin
 		# Try to change directory.
@@ -24,12 +27,27 @@ images = []
 # Enter your excluded folders and images here. 
 # Folders: External libraries and classes, for example.
 # Images: Icons and loading screens, for example.
-excluded = ['vendor', 'default', 'icon']
+excluded_folders = ['vendor', '.appiconset', '.launchimage']
+excluded_images = []
+imagesets = ['.imageset']
 # Create an array with only the image names but not the ones under excluded folders.
 image_paths.each do |path|
 	do_next = false
-	excluded.each do |e|
-		if path.to_s.downcase.include?(e)
+	excluded_folders.each do |e|
+		if path.to_s.include?(e) && File.basename(path, '.*').to_s != e
+			do_next = true
+			break
+		end
+	end
+	excluded_images.each do |e|
+		if File.basename(path, '.*').to_s == e
+			do_next = true
+			break
+		end
+	end
+	imagesets.each do |i|
+		if path.to_s.include?(i)  && File.basename(path, '.*').to_s != i
+			images << /(\/.+)\/(.+)(#{i})\//.match(path.to_s.downcase)[2]
 			do_next = true
 			break
 		end
@@ -37,23 +55,30 @@ image_paths.each do |path|
 	next if do_next
 	images << File.basename(path, '.*').to_s
 end
+images.uniq!
+images.sort_by! { |m| m.downcase }
+p images if VERBOSE
 # Trim '@2x' if it exists.
 images.each do |image|
 	images.delete(image) if image.include?("@2x")
 end
 # Search for occurences of each image name in all our files.
-# NOTE: We WILL search within excluded folders for YOUR images,
+# NOTE: It WILL search within excluded folders for YOUR images,
 # since there's the chance you have modified external libraries with your own images.
 files.each do |file|
-	puts file
-	# Remove from the images array what we want to keep. Anything we find in files, that is.
+	p file if VERBOSE
+	# Remove from the images array what we find: these will be kept.
 	images.delete_if do |image|
 		unless File.directory?(file)
-			# Just the start of the string, because an image can be defined with imageNamed: @"image",
-			# but also with imageNamed: @"image.png", or even imageNamed: @"image@2x.png".
-			# We want to cover everything.
-			if File.read(file).downcase.include?('@"' + image.downcase)
-				puts image
+			# An image can be defined with 
+			# imageNamed: @"image", imageNamed: @"image.png", or imageNamed: @"image@2x.png".
+			if File.read(file).include?('imageNamed:@"' + image + '@2x.png"') or
+				File.read(file).include?('imageNamed:@"' + image + '.png"') or
+				File.read(file).include?('imageNamed:@"' + image + '"') or
+				File.read(file).include?('imageNamed: @"' + image + '@2x.png"') or
+				File.read(file).include?('imageNamed: @"' + image + '.png"') or
+				File.read(file).include?('imageNamed: @"' + image + '"')
+				p image if VERBOSE
 				true
 			end
 		end
@@ -64,9 +89,10 @@ files_to_be_deleted = []
 Dir['**/*.*'].each do |file|
 	images.each do |image|
 		# Check if the last path component equals either the image name, or the image name + @2x, since we excluded those from the initial search.
-		if File.basename(file, '.*').to_s.downcase == image.downcase or
-			File.basename(file, '.*').to_s.downcase == image.downcase + '@2x' or
-			File.basename(file, '.*').to_s.downcase == image.downcase + '.imageset'
+		# If the last path component does equal the image name BUT the file path also includes the containing imageset, don't remove it, we will just remove the imageset altogether
+		if (File.basename(file, '.*').to_s == image and !file.to_s.include?('.imageset')) or
+			(File.basename(file, '.*').to_s.downcase == image + '@2x' and !file.to_s.include?('.imageset')) or
+			File.basename(file).to_s == image + '.imageset'
 			files_to_be_deleted << file
 			break
 		end
@@ -82,40 +108,63 @@ unless files_to_be_deleted.count == 0
 	deleted_files = []
 	imagesets_to_delete = {}
 	files_to_be_deleted.sort_by{ |m| m.downcase }.each do |file|
+		image_to_remove_from_project = ''
+		pbxproj = nil
+		lines = []
+		# Pbxproj doesn't need updating when deleting imagesets, it updates the project automatically.
+		if File.extname(file) != '.imageset'
+			Dir['*/*.*'].each do |f|
+				if File.extname(f) == '.pbxproj'
+					image_to_remove_from_project = File.basename(file)
+					# If pbxproj contains the file we are asking to delete.
+					if File.read(f).include?(image_to_remove_from_project)
+						# Read the contents of pbxproj into an array.
+						lines = File.readlines(f)
+						lines.delete_if do |l|
+							# Delete the line that contains the file we are asking to delete;
+							# it will rewrite pbxproj in the next step, only if you choose yes or delete
+							true if l.include?(image_to_remove_from_project)
+						end
+						pbxproj = f
+					end
+				end
+			end
+		end
 		if answer.downcase == 'd' or answer.downcase == 'delete'
 			deleted_files << file
-			FileUtils.rm_f(file)
+			FileUtils.rm_rf(file)
+			# Pbxproj doesn't need updating when deleting imagesets, it updates the project automatically.
+			if File.extname(file) != '.imageset'
+				File.open(pbxproj, 'w') do |f|
+					f.write(lines.join(''))
+				end
+			end
 		elsif answer.downcase == 'o' or answer.downcase == 'one by one'
 			puts 'Delete ' + file + '? y(es)/n(o)'
 			individual_answer = STDIN.gets.chomp
 			if individual_answer == 'y' or individual_answer == 'yes'
 				deleted_files << file
-				if File.directory?(file)
-					# Maybe we don't want to delete some imagesets.
-					imagesets_to_delete[file.to_sym] = 'yes'
-				else
-					FileUtils.rm_f(file)
+				FileUtils.rm_rf(file)
+				# Pbxproj doesn't need updating when deleting imagesets, it updates the project automatically.
+				if File.extname(file) != '.imageset'
+					File.open(pbxproj, 'w') do |f|
+						f.write(lines.join(''))
+					end
 				end
 			end
 		end
 	end
-	# Iterate again to check for empty imageset folders.
-	files_to_be_deleted.sort_by{ |m| m.downcase }.each do |file|
-		if File.directory?(file) and
-			Dir.entries(file).count == 3 and
-			Dir.entries(file)[0] == "." and
-			Dir.entries(file)[1] == ".." and
-			Dir.entries(file)[2] == "Contents.json" and
-			imagesets_to_delete[file.to_sym] == 'yes' # Delete only what was chosen to be deleted on the previous step.
-			FileUtils.rm_r(file)
-		end
-	end
 	unless answer == 'c' or answer == 'cancel'
-		puts '---'
-		puts 'Files deleted:'
-		puts deleted_files
-		puts '---'
-		puts 'Done'
+		unless deleted_files.count == 0
+			puts '---'
+			puts 'Files deleted:'
+			puts deleted_files
+			puts '---'
+			puts 'Done'
+		else
+			p '---'
+			p 'No files harmed.'
+		end
 	end
 else
 	puts '---'
